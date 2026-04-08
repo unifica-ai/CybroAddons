2f995f9273e045cac950b14f69541392a033ce19
Kyle S Passarelli <kyle.passarelli@gmail.com>
2026-04-07 20:14:25 -0600

    Rebrand

--- code_backend_theme/static/src/js/fields/colors.js
+++ code_backend_theme/static/src/js/fields/colors.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 import { getColor } from "@web/views/graph/colors";
 
-var code_backend_color = ["#556ee6", "#f1b44c", "#50a5f1", "#ffbb78", "#34c38f", "#98df8a", "#d62728",
+var code_backend_color = ["#3b5cff", "#f1b44c", "#11a8ff", "#ffbb78", "#34c38f", "#98df8a", "#d62728",
         "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2",
         "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"];
 
